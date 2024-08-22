0f75f2869764076e313b08b570499776ba6c7f81
J031 <94027313+Joel-Sullivan@users.noreply.github.com>
2024-08-22 12:40:11 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,31 +1,46 @@
-function loadSidebar() {
-    fetch('sidebar.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('sidebar').innerHTML = data;
-            // Initialize toggleNav after loading the sidebar
-            var menuButton = document.getElementById('navbtn');
-            if (menuButton) {
-                menuButton.addEventListener('click', toggleNav);
-            }
-
-            var closeButton = document.querySelector('.closebtn');
-            if (closeButton) {
-                closeButton.addEventListener('click', toggleNav);
-            }
-        })
-        .catch(error => console.error('Error loading sidebar:', error));
-}
-
-function toggleNav() {
-    var sidebar = document.getElementById('sidebar');
-    if (sidebar) {
-        sidebar.classList.toggle('show');
-    } else {
-        console.error("Element with ID 'sidebar' not found");
-    }
-}
-
 document.addEventListener('DOMContentLoaded', function() {
-    loadSidebar(); // Call loadSidebar when the DOM is ready
+  // Toggle Navigation Function
+  function toggleNav() {
+      var sidebar = document.getElementById('sidebar');
+      if (sidebar) {
+          sidebar.classList.toggle('show');
+      } else {
+          console.error("Element with ID 'sidebar' not found");
+      }
+  }
+
+  // Load Sidebar Function
+  function loadSidebar() {
+      fetch('sidebar.html')
+          .then(response => response.text())
+          .then(data => {
+              var sidebarContainer = document.getElementById('sidebar-container');
+              sidebarContainer.innerHTML = '';
+              sidebarContainer.innerHTML = data;
+
+              var menuButton = document.getElementById('navbtn');
+              if (menuButton) {
+                  menuButton.addEventListener('click', toggleNav);
+              }
+
+              var closeButton = document.querySelector('.closebtn');
+              if (closeButton) {
+                  closeButton.addEventListener('click', toggleNav);
+              }
+          })
+          .catch(error => console.error('Error loading sidebar:', error));
+  }
+
+  loadSidebar(); // Load sidebar when DOM content is loaded
+
+  // Theme Toggle Function
+  function toggleTheme() {
+      document.documentElement.classList.toggle('dark-theme');
+  }
+
+  // Attach the toggleTheme function to the button
+  var themeToggleButton = document.getElementById('theme-toggle');
+  if (themeToggleButton) {
+      themeToggleButton
+  }
 });
